8a3e2907abb1c8d14eb030af196639d9f39f818b
priscila andreani <priscilaandreani@gmail.com>
2022-01-31 17:52:47 -0300

    add components: access, banner, login and signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccessComponent } from './access/access.component';
+import { BannerComponent } from './access/banner/banner.component';
+import { LoginComponent } from './access/login/login.component';
+import { SignupComponent } from './access/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccessComponent,
+    BannerComponent,
+    LoginComponent,
+    SignupComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
